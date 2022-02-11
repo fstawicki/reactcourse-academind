5d23fef9b9eabd44a12b0093475efd5276e72005
fstawicki <stawickifilip1@gmail.com>
2022-02-12 00:25:27 +0100

    how react works under the hood; React.memo; callbacks

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,28 +1,22 @@
-import React, { useState } from 'react';
-import Button from './components/UI/Button/Button'
+import React, { useState, useCallback, useMemo } from 'react';
 
 import './App.css';
-import DemoOutput from './components/Demo/DemoOutput';
+import DemoList from './components/Demo/DemoList';
+import Button from './components/UI/Button/Button';
 
 function App() {
+  const [listTitle, setListTitle] = useState('My List');
 
-  const [showParagraph, setShowParagraph] = useState(false);
+  const changeTitleHandler = useCallback(() => {
+    setListTitle('New Title');
+  }, []);
 
-  const toggleParagraph = () => {
-    setShowParagraph(true);
-  }
+  const listItems = useMemo(() => [5, 3, 1, 10, 9], []);
 
   return (
     <div className="app">
-      <h1>Hi there!</h1>
-      
-      {/* {showParagraph && <p>this is new@!@@@</p>} */}
-
-      <DemoOutput show={showParagraph} />
-
-      <Button onClick={toggleParagraph} >Show PAragraph</Button>
-      
-
+      <DemoList title={listTitle} items={listItems} />
+      <Button onClick={changeTitleHandler}>Change List Title</Button>
     </div>
   );
 }
