e0dfbf89ed6e2448e990159e28a46d31e72dae27
fstawicki <stawickifilip1@gmail.com>
2022-02-14 00:27:03 +0100

    building class based components

--- 3/src/App.js
+++ 3/src/App.js
@@ -1,11 +1,21 @@
+import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
 
-import './App.css';
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
 
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+
   return (
-    <div className="App">
-     
-    </div>
+    <UsersContext.Provider value={usersContext}>
+      <UserFinder />
+    </UsersContext.Provider>
   );
 }
 
