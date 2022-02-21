d8586539d133cc9c7fd6cea8d240230e07051dcf
fstawicki <stawickifilip1@gmail.com>
2022-02-21 23:37:28 +0100

    connecting to database, modifing website

--- 8/src/components/Meals/AvailableMeals.js
+++ 8/src/components/Meals/AvailableMeals.js
@@ -8,11 +8,21 @@ import {useEffect, useState} from 'react';
 const AvailableMeals = () => {
 
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+
 
   useEffect( () => {
 
     const fetchData = async () => {
+      
       const response = await fetch('https://test-165d9-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+
+      if(!response.ok){
+        throw new Error('something went wrong :(');
+      }
+
       const data = await response.json();
     
       const loadedMeals = [];
@@ -30,11 +40,37 @@ const AvailableMeals = () => {
 
     }
     
-    fetchData();
+
+    
+      fetchData().catch(error => {
+        setIsLoading(false);
+        setHttpError(error.message);
+
+      });
+    
+      
+
+    setIsLoading(false);
     
     
     
   }, []);
+
+  if(isLoading){
+    return(
+      <section className='MealsLoading'>
+        <p>Loading meals, please wait...</p>
+      </section>
+    )
+  }
+
+  if(httpError){
+    return(
+      <section className='MealsLoading'>
+        <p>{httpError}</p>
+      </section>
+    )
+  }
   
   const mealsList = meals.map((meal) => (
     <MealItem
