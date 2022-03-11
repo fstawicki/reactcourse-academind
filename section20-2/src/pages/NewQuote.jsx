fa063045710aa80fb6b20d211fc4fcea11f61ff7
fstawicki <stawickifilip1@gmail.com>
2022-03-11 23:27:37 +0100

    useHistory and Prompt component

--- section20-2/src/pages/NewQuote.jsx
+++ section20-2/src/pages/NewQuote.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 function NewQuote() {
 
+  const history = useHistory();
+
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
 
+    //push dodaje nową stronę na stack więc można wrócić na poprzednią
+    //replace zamioenia stronę więc nie można z niej wrócić
+    history.push('/quotes')
   }
 
   return (
