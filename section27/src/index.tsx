b6e2d8a7436d61609d73ddb828a8c305026b621a
fstawicki <stawickifilip1@gmail.com>
2022-04-20 22:12:56 +0200

    Learning Typescript Basics

--- section27/src/index.tsx
+++ section27/src/index.tsx
@@ -4,8 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
     <App />
@@ -16,3 +17,13 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+// import './index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
