4283b1af9a2710df027541d63ce9c5542a595924
fstawicki <stawickifilip1@gmail.com>
2022-04-20 22:43:07 +0200

    Learning Typescript Basics

---
+++ section27/src/components/NewTodo.tsx
@@ -0,0 +1,24 @@
+import React, {useRef} from 'react'
+
+
+const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const enteredText = todoTextInputRef.current!.value;
+
+        props.onAddTodo(enteredText);
+    }
+
+  return (
+    <form onSubmit={submitHandler}>
+        <label htmlFor="text">Todo text</label>
+        <input type="text" id="text" ref={todoTextInputRef} />
+        <button>Add Todo</button>
+    </form>
+  )
+}
+
+export default NewTodo
